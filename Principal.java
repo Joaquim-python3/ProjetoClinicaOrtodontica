80bf7c4502db1489b2ef7f3d7fe6a1362ae3e3ba
Joaquim Cabral <67121558+Joaquim-python3@users.noreply.github.com>
2022-02-11 20:42:39 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,6 +2,9 @@ package Menu;
 
 public class Principal {
 	public static void main(String[] args) {
+		Agendamento ag = new Agendamento();
+		ag.criarPlanilha();
+
 		Menu menu = new Menu();
 		
 		menu.menuPrincipal();
