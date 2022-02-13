1f374d947ef88b9ebac57ee87f03eaea046aa956
Joaquim Cabral <67121558+Joaquim-python3@users.noreply.github.com>
2022-02-13 14:55:54 -0300

    Update UsarCliente.java

--- UsarCliente.java
+++ UsarCliente.java
@@ -1,6 +1,7 @@
 package Menu;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 
 import Menu.Plano.tipoPlano;
@@ -8,33 +9,32 @@ import Menu.Plano.tipoPlano;
 public class UsarCliente {
 
 	Scanner ent = new Scanner(System.in);
-	ArrayList<Plano> lista = new ArrayList<>();
-	Cliente c = new Plano();
-	
-	boolean plano;
-	String nomeApoio, cpfApoio, dataNascApoio, comorbidadesApoio;
+	Collection<Cliente> listaC = new ArrayList<>();
+
 	int n;
-	
-	public void cadastrarCliente() {
+	boolean plano;
 
-		System.out.println("Nome completo do cliente: ");
-		nomeApoio = ent.next();
-		c.setNome(nomeApoio);
-
-		System.out.println("CPF do cliente: ");
-		cpfApoio = ent.next();
-
-		System.out.println("Data de nascimento: ");
-		dataNascApoio = ent.next();
-
-		System.out.println("Comorbidade:");
-		comorbidadesApoio = ent.next();
-
-		System.out.println("O cliente deseja usar plano?\n1 - Sim\t2- Não");
-		n = ent.nextInt();
+	public Cliente cadastrarCliente() {
 		
+		Scanner ent = new Scanner(System.in);
+
+		System.out.print("Nome do cliente: ");
+		String nome = ent.nextLine();
+
+		System.out.print("CPF do cliente: ");
+		String cpf = ent.next();
+		
+		System.out.print("Data de nascimento: ");
+		String dataNascimento = ent.next();
+
+		System.out.print("Comorbidade:");
+		String comorbidade = ent.next();
+
+		System.out.println("O cliente deseja usar plano?\n1 - Sim\t2 - Não");
+		n = ent.nextInt();
+
 		if (n == 1) {
-			
+
 			plano = true;
 
 			System.out.println("Qual o tipo de plano?\n1 - Familiar\t2 - Empresarial");
@@ -66,71 +66,34 @@ public class UsarCliente {
 			System.out.println("Valor inválido");
 
 		}
+
+		Cliente c = new Cliente(nome, cpf, dataNascimento, comorbidade, plano);
+
+		listaC.add(c); // adicionando objeto cliente
 		
-		Plano c = new Plano(nomeApoio, cpfApoio, dataNascApoio, comorbidadesApoio, plano);
-		lista.add(c);
-		
+		return c;
 	}
-	
+
+	public Collection<Cliente> getListaC() {
+		return listaC;
+	}
+
 	public void agendarConsulta() {
-		
-        Agendamento ag = new Agendamento();
-        
-        System.out.println("");
 
-        System.out.println("Qual dia da semana que deseja realizar a consulta?"
-                + "\n0 - Segunda\n1 - Terça\n2 - Quarta\n3 - Quinta\n4 - Sexta");
-        int dia = ent.nextInt();
+		Agendamento ag = new Agendamento();
 
+		System.out.println("");
 
-        System.out.println("Digite o número do melhor horário para você: "
-                + "\n0 - 8:00\n1 - 10:00\n2 - 14:00\n3 - 16:00\n4 - 18:00\n5 - 20:00");
-        int g = ent.nextInt();
+		System.out.println("Qual dia da semana que deseja realizar a consulta?"
+				+ "\n0 - Segunda\n1 - Terça\n2 - Quarta\n3 - Quinta\n4 - Sexta");
+		int dia = ent.nextInt();
 
-        ag.agendar(dia,g);
-		
+		System.out.println("Digite o número do melhor horário para você: "
+				+ "\n0 - 8:00\n1 - 10:00\n2 - 14:00\n3 - 16:00\n4 - 18:00\n5 - 20:00");
+		int g = ent.nextInt();
+
+		ag.agendar(dia, g);
+
 	}
 
-	// GETTERS E SETTERS ()
-	public boolean isPlano() {
-		return plano;
-	}
-
-	public void setPlano(boolean plano) {
-		this.plano = plano;
-	}
-
-	public String getNomeApoio() {
-		return nomeApoio;
-	}
-
-	public void setNomeApoio(String nomeApoio) {
-		this.nomeApoio = nomeApoio;
-	}
-
-	public String getCpfApoio() {
-		return cpfApoio;
-	}
-
-	public void setCpfApoio(String cpfApoio) {
-		this.cpfApoio = cpfApoio;
-	}
-
-	public String getDataNascApoio() {
-		return dataNascApoio;
-	}
-
-	public void setDataNascApoio(String dataNascApoio) {
-		this.dataNascApoio = dataNascApoio;
-	}
-
-	public String getComorbidadesApoio() {
-		return comorbidadesApoio;
-	}
-
-	public void setComorbidadesApoio(String comorbidadesApoio) {
-		this.comorbidadesApoio = comorbidadesApoio;
-	}
-	
-	
 }
