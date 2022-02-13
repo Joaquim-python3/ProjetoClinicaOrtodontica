387e4aaae1f38c170311be61099abeca540e5360
Joaquim Cabral <67121558+Joaquim-python3@users.noreply.github.com>
2022-02-13 09:40:43 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class Menu {
 
-	ArrayList<Plano> listaPlanos = new ArrayList<>();
-	
+	ArrayList<Plano> lista = new ArrayList<>();
+
 	Scanner ent = new Scanner(System.in);
 	int opcao;
 	UsarCliente mCliente = new UsarCliente();
@@ -19,7 +19,7 @@ public class Menu {
 		System.out.println("| 1 - Serviços |"); // mostrar o plano, oque se pode fazer
 		System.out.println("| 2 - Cliente |"); // informacoes do cliente
 		System.out.println("| 3 - Funcionarios |"); // pegar informações do func // classe abstrata com heranca
-		System.out.println("| 4 - Agendamento de consulta |"); // pedir o nome e selecionar data da consulta 
+		System.out.println("| 4 - Agendamento de consulta |"); // pedir o nome e selecionar data da consulta
 		System.out.println("| 5 - Sair |");
 
 		System.out.println("Digite uma opção: ");
@@ -29,18 +29,21 @@ public class Menu {
 		case 1:
 			servicos();
 			break;
-			
+
 		case 2:
 			menuCliente();
 			break;
-			
+
 		case 3:
 			funcionarios();
 			break;
-			
+
 		case 4:
 			mCliente.agendarConsulta();
 			menuPrincipal();
+			break;
+		case 5:
+
 			break;
 		}
 	}
@@ -48,14 +51,14 @@ public class Menu {
 	// SERVICOS
 	public void servicos() {
 
-		System.out.println("##--MENU SERVICOS--##\\n\\n\"");
+		System.out.println("##--MENU SERVICOS--##\n\n\"");
 		System.out.println("| 1 - Clareamento |");
 		System.out.println("| 2 - Procedimento estético |");
 		System.out.println("| 3 - Remoção de cárie |");
 		System.out.println("| 4 - Revisão Geral |"); // pergunta oque o paciente quer de especifico e pede um
 		System.out.println("| 5 - Sair |");
-		
-		// esse servicos 
+
+		// esse servicos
 		System.out.println("Digite uma opção: ");
 		opcao = ent.nextInt();
 
@@ -73,19 +76,16 @@ public class Menu {
 
 			break;
 		case 5:
-
-			break;
-		case 6:
-
 			menuPrincipal();
 			break;
+
 		}
 
 	}
 
 	public void menuCliente() {
 
-		System.out.println("##--MENU CLIENTE--##\\n\\n\"");
+		System.out.println("##--MENU CLIENTE--##\n\n");
 		System.out.println("| 1 - Cadastrar Cliente |");
 		System.out.println("| 2 - Remover cliente |");
 		System.out.println("| 3 - Consultas de todos os clientes |"); // vai mostrar todas as consultas
@@ -98,49 +98,68 @@ public class Menu {
 		switch (opcao) {
 
 		case 1:
-			
+
 			mCliente.cadastrarCliente();
+
 			menuCliente();
 			break;
-			
+
 		case 2:
-			
+
 			System.out.println("Digite o nome do cliente: ");
 			String nomeParaRemover = ent.next();
-			
-			Plano plano = new Plano();
-			
-			for (Plano p : listaPlanos) {
-				if(p.getNome().equalsIgnoreCase(nomeParaRemover)) {
+
+			Cliente plano = new Plano();
+
+			for (Cliente p : lista) {
+				if (p.getNome().equalsIgnoreCase(nomeParaRemover)) {
 					plano = p;
 				}
 			}
-			
-			listaPlanos.remove(plano);
+
+			lista.remove(plano);
 			menuCliente();
 			break;
-			
+
 		case 3:
-			
+
 			ag.horariosDisponiveis();
 			break;
-			
+
 		case 4:
+
+//			System.out.print("Digite um nome: ");
+//			String nomeAjuda = ent.next();
+//			System.out.println("");
 			
-			for(Plano p : listaPlanos) {
-				System.out.println(p);
+			for (Plano p : lista) {
+				
+				System.out.println("nomeApoios=" + mCliente.getNomeApoio());
+				System.out.println("cpfApoio=" + mCliente.getCpfApoio());
+				System.out.println("dataNascApoio=" + mCliente.getDataNascApoio());
+				System.out.println("********************************");
+				System.out.println("nome="+p.getNome());
+				System.out.println("cpf="+p.getCpf());
+				System.out.println("dataNascimento="+p.getDataNascimento());
 			}
-			
+
+			// System.out.println(mCliente.getNomeApoio());//mCliente faz parte da classe
+			// UsarCliente,
+			// nomeApoio serve como um apoio, pq tava retornando null se usar p.getNome
+			menuPrincipal();
 			break;
+
+		case 5:
+			menuPrincipal();
 		}
 
 	}
-	
+
 	public void funcionarios() {
 
 		System.out.println("Digite o codigo do funcionario: ");
 		String cdgFuncionario = ent.next(); // quando digitar vai aparacer os subopcoes para Funcionario
-		
+
 	}
 
 }
