61ea548aef5a94f7f1dbce23dafd876e6142a255
Joaquim Cabral <67121558+Joaquim-python3@users.noreply.github.com>
2022-02-13 18:44:16 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -7,7 +7,9 @@ public class Menu {
 	Scanner ent = new Scanner(System.in);
 	int opcao;
 	UsarCliente mCliente = new UsarCliente();
+	UsarFuncionario mFuncionario = new UsarFuncionario();
 	Agendamento ag = new Agendamento();
+	Plano p = new Plano();
 
 	public void menuPrincipal() {
 
@@ -18,21 +20,21 @@ public class Menu {
 		System.out.println("| 3 - Funcionarios |"); // pegar informações do func // classe abstrata com heranca
 		System.out.println("| 4 - Agendamento de consulta |"); // pedir o nome e selecionar data da consulta
 		System.out.println("| 5 - Sair |");
-
+		
 		System.out.println("Digite uma opção: ");
 		opcao = ent.nextInt();
-
+		
 		switch (opcao) {
 		case 1:
 			servicos();
 			break;
-
+			
 		case 2:
 			menuCliente();
 			break;
-
+			
 		case 3:
-			funcionarios();
+			menuFuncionarios();
 			break;
 
 		case 4:
@@ -48,36 +50,24 @@ public class Menu {
 	// SERVICOS
 	public void servicos() {
 
-		System.out.println("##--MENU SERVICOS--##\n\n");
-		System.out.println("| 1 - Clareamento |");
-		System.out.println("| 2 - Procedimento estético |");
-		System.out.println("| 3 - Remoção de cárie |");
-		System.out.println("| 4 - Revisão Geral |"); // pergunta oque o paciente quer de especifico e pede um
-		System.out.println("| 5 - Sair |");
+		System.out.println("##--SERVICOS--##\n\n");
+		System.out.println("| Diagnóstico|");
+		System.out.println("| Prevenção |");
+		System.out.println("| Odontopediatria |");
+		System.out.println("| Dentística |"); // pergunta oque o paciente quer de especifico e pede um
+		System.out.println("| Endodontia |");
+		System.out.println("| Periodontia |");
+		System.out.println("| Prótese |");
+		System.out.println("| Cirurgia |");
+		System.out.println("| Ortodontia |");
+		System.out.println("| Radiologia |");
+		System.out.println("| 1 - Sair |");
 
-		// esse servicos
-		System.out.println("Digite uma opção: ");
+		System.out.println("Digite 1 para sair: ");
 		opcao = ent.nextInt();
-
-		switch (opcao) {
-		case 1:
-
-			break;
-		case 2:
-
-			break;
-		case 3:
-
-			break;
-		case 4:
-
-			break;
-		case 5:
+		if (opcao == 1) {
 			menuPrincipal();
-			break;
-
 		}
-
 	}
 
 	public void menuCliente() {
@@ -85,7 +75,7 @@ public class Menu {
 		System.out.println("##--MENU CLIENTE--##\n\n");
 		System.out.println("| 1 - Cadastrar Cliente |");
 		System.out.println("| 2 - Remover cliente |");
-		System.out.println("| 3 - Consultas de todos os clientes |"); // vai mostrar todas as consultas
+		System.out.println("| 3 - Horários disponíveis |"); // vai mostrar todas as consultas
 		System.out.println("| 4 - Consultar |");
 		System.out.println("| 5 - Sair");
 
@@ -102,53 +92,65 @@ public class Menu {
 			break;
 
 		case 2:
-			
+
 			Cliente ajuda = new Cliente();
-			
+
 			System.out.println("Digite o nome do cliente: ");
 			String nomeParaRemover = ent.next();
 
 			for (Cliente c : mCliente.getListaC()) {
-				
+
 				if (c.getNome().equalsIgnoreCase(nomeParaRemover)) {
 					ajuda = c;
-					
+
 				}
-				
+
 			}
 
 			mCliente.getListaC().remove(ajuda);
+			System.out.println("Cliente removido com sucesso");
 			menuCliente();
 			break;
 
 		case 3: // mostrar os horarios disponiveis
 
 			ag.horariosDisponiveis();
+			menuPrincipal();
 			break;
 
 		case 4: // consultar
-			
-			
-			
-			for (Cliente cliente : mCliente.getListaC()) { // podemos chamar isso de gambiarra?
-				
-				System.out.println("Digite um nome: ");
-				String nomeConsultar = ent.next();
-				
-				if(cliente.getNome().equalsIgnoreCase(nomeConsultar)) {
-					
-					System.out.println("nome="+cliente.getNome());
-					System.out.println("cpf="+cliente.getCpf());
-					System.out.println("comorbidades="+cliente.getComorbidades());
-					System.out.println("tem plano= "+cliente.isPlano());
+
+			System.out.println("Digite um nome: ");
+			String nomeConsultar = ent.next();
+
+			for (Plano cliente : mCliente.getListaC()) { // podemos chamar isso de gambiarra?
+
+				if (cliente.getNome().equalsIgnoreCase(nomeConsultar)) {
+
+					System.out.println("nome=" + cliente.getNome());
+					System.out.println("cpf=" + cliente.getCpf());
+					System.out.println("comorbidades=" + cliente.getComorbidades());
+
+					if (cliente.isPlano() == true) {
+
+						if (mCliente.getPlanoVar() == 1) {
+
+							System.out.println("tipo do plano= familiar");
+
+						} else if (mCliente.getPlanoVar() == 2) {
+
+							System.out.println("tipo do plano= empresarial");
+
+						}
+
+						// isso aqui é um erro, resolver com a professora
+						System.out.println("quantidade de pessoas no plano=" + cliente.getQuantidadeDePessoas());
+
+					}
 					System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-");
-					
-				}else{ 
-					
-					System.out.println("nenhuma pessoa com esse nome foi cadastrado");
-					
+
 				}
-				
+
 			}
 
 			menuPrincipal();
@@ -156,15 +158,100 @@ public class Menu {
 
 		case 5:
 			menuPrincipal();
+			break;
 		}
 
 	}
 
-	public void funcionarios() {
+	public void menuFuncionarios() {
 
-		System.out.println("Digite o codigo do funcionario: ");
-		ent.next();
+		System.out.println("##--MENU FUNCIONARIO--##\n\n");
+		System.out.println("| 1 - Cadastrar funcionario |");
+		System.out.println("| 2 - Remover funcionario |");
+		System.out.println("| 3 - Pagamento em assiduidade |");
+		System.out.println("| 4 - Consultar |");
+		System.out.println("| 5 - Sair");
 
+		System.out.println("Digite uma opcao: ");
+		int opcao = ent.nextInt();
+
+		switch (opcao) {
+		case 1:
+
+			mFuncionario.cadastrarFuncionario();
+			menuFuncionarios();
+			break;
+
+		case 2:
+
+			Funcionario ajudaFunc = new Funcionario();
+
+			System.out.println("Digite um nome para ");
+			String nomeRemoverFunc = ent.next();
+
+			for (Funcionario f : mFuncionario.getListaFuncionario()) {
+
+				if (f.getNome().equalsIgnoreCase(nomeRemoverFunc)) {
+					ajudaFunc = f;
+
+				}
+
+			}
+
+			mFuncionario.getListaFuncionario().remove(ajudaFunc);
+			menuPrincipal();
+			break;
+
+		case 3: // assiduidade
+
+			System.out.println("Digite o código do funcionário: ");
+			int cdg1 = ent.nextInt();
+
+			for (Funcionario func : mFuncionario.getListaFuncionario()) {
+
+				if (func.getCodigo() == cdg1) {
+
+					System.out.println("Quantidade de dias do funcionário presente no trabalho? \nDias úteis no mês: 20");
+					int qtdDias = ent.nextInt();
+
+					System.out.println("Salário líquido do mês: " + mFuncionario.pagamentoAssiduidade(qtdDias));
+
+				} else {
+					System.out.println("Valor inválido");
+				}
+
+			}
+
+			menuPrincipal();
+			break;
+
+		case 4: // consultar
+
+			System.out.println("Digite o código do funcionário: ");
+			int cdg = ent.nextInt();
+
+			for (Funcionario func : mFuncionario.getListaFuncionario()) {
+
+				if (func.getCodigo() == cdg) {
+
+					System.out.println("nome=" + func.getNome());
+					System.out.println("salario=" + func.getSalario());
+					System.out.println("cpf=" + func.getCpf());
+
+				}
+			}
+
+			menuPrincipal();
+			break;
+		case 5:
+
+			menuPrincipal();
+			break;
+		default:
+
+			System.out.println("erro");
+			break;
+		}
 	}
 
 }
